7d58473db06d449159012190f94fdb1518b47d1e
Najjad <80614053+Najjad@users.noreply.github.com>
2021-03-16 11:11:14 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,91 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+import os
+#from datetime import datetime
+
+client = commands.Bot(command_prefix="!")
+
+#now = datetime.now() (will be used soon)
+#current_time = now.strftime("%H")
+p#rint("Current time: ", current_time)
+
+
+
+@client.command()
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+    await voiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused.")
+
+
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+credits = "Made with awesomeness by Nij"
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('credits'):
+        await message.channel.send(credits)
+
+
+client.run('ODE3MDc1MDc0MjQxNjU4OTIw.YEEOdg.UHbp49Xd8maDRr3MHY_GZ6wjC2k')
+
+#brew install opus
+#pip install youtube-dl
+#pip install ffmpeg and ffmprobe (download the file from the website as well, extract it, then put the executable in the bin folder)
